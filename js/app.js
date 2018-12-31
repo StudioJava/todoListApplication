f430958e16f19b89174a0be4ea9d21ae5abd1865
StudioJava <38365682+StudioJava@users.noreply.github.com>
2018-12-31 13:49:56 -0400

    connection to buttons and adding event listeners

--- js/app.js
+++ js/app.js
@@ -37,5 +37,40 @@
         todo.completed = !todo.completed;
         this.displayTodos();
 
+    },
+    toggleAll: function(){
+        var totalTodos = this.todos.length;
+        var completedTodos = 0;
+        //Get number of completed todos
+        for(var i =0;i < totalTodos;i++){
+            if(this.todos[i].completed === true){
+                completedTodos++;
+            }
+        }
+        if(completedTodos === totalTodos){
+            for(var i = 0;i < totalTodos;i++){
+                this.todos[i].completed = false;
+            }
+        }else{
+            for(var i =0;i < totalTodos;i++){
+                this.todos[i].completed = true;
+            }
+        }
+        this.displayTodos();
     }
 };
+//+++++++++++++++ 
+var displayTodosButton = document.getElementById("displayTodosBtn");
+var toggleAllButton = document.getElementById("toggleAllBtn");
+
+//++++++++++++++
+displayTodosButton.addEventListener('click',function(){
+	todoList.displayTodos();
+});
+toggleAllButton.addEventListener('click',function(){
+	todoList.toggleAll();
+});
+
+
+
+
